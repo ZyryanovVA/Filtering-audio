7b478b99dfa81e330aa84b5fb183f9287ae2a839
ZyryanovVA <zyryanov.v.2.0.0.0@mail.ru>
2022-09-28 15:36:11 +0300

    Audio-Filtering

---
+++ Praktika/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿
+namespace Praktika
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(172, 33);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(146, 22);
+            this.dateTimePicker1.TabIndex = 0;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(172, 86);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(146, 22);
+            this.dateTimePicker2.TabIndex = 1;
+            this.dateTimePicker2.Value = new System.DateTime(2021, 7, 29, 0, 0, 0, 0);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(403, 35);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(96, 21);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "Входящие";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(403, 85);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(104, 21);
+            this.checkBox2.TabIndex = 3;
+            this.checkBox2.Text = "Исходящие";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.SaddleBrown;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(29, 118);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 33);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Выбор папки";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(726, 33);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(130, 22);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(726, 83);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(130, 22);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 17);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Дата начала";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(127, 17);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Дата завершения";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(575, 38);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(140, 17);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Количество файлов";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(575, 86);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(136, 17);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Длительность (МБ)";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(172, 123);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(684, 23);
+            this.richTextBox1.TabIndex = 11;
+            this.richTextBox1.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.SaddleBrown;
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.Location = new System.Drawing.Point(29, 162);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(827, 28);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Показать";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(29, 219);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(827, 244);
+            this.listBox1.TabIndex = 13;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Tan;
+            this.ClientSize = new System.Drawing.Size(882, 495);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Записи звонков";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Praktika/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace Praktika
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            dateTimePicker1.MaxDate = DateTime.Today;
+            dateTimePicker2.MaxDate = DateTime.Today;
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string filetext = folderBrowserDialog1.SelectedPath;
+                richTextBox1.Text = filetext;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+                listBox1.Items.Clear(); 
+                if (richTextBox1.Text == String.Empty)
+                {
+                    listBox1.Items.Add("Выберите папку с файлами");
+                    return;
+                }
+                string filetext = richTextBox1.Lines[0];
+                DirectoryInfo dir = new DirectoryInfo(filetext);
+                FileInfo[] files = dir.GetFiles("*.wav");
+                int dlinamas = files.Length;
+                if (dlinamas == 0)
+                {
+                    listBox1.Items.Add("В этой папке нет файлов с расширением wav");
+                    return;
+                }
+                var result3 = DateTime.Compare(dateTimePicker1.Value, dateTimePicker2.Value);
+                if (result3 > 0)
+                {
+                    listBox1.Items.Add("Введите даты корректно");
+                    return;
+                }
+                if (textBox1.Text == String.Empty) {
+                        foreach (FileInfo fi in files)
+                        {
+                            string a = fi.ToString();
+                            int indexOfChar = a.IndexOf("QUEUE%");
+                            DateTime time1 = fi.CreationTime;
+                            string time1s = time1.ToShortDateString();
+                            var result1 = DateTime.Compare(time1, dateTimePicker1.Value);
+                            var result2 = DateTime.Compare(time1, dateTimePicker2.Value);
+                            string picktime2 = dateTimePicker2.Value.ToShortDateString();
+                            long dlit = fi.Length / 1024 / 1024;
+                            if ((result1 >= 0 && result2 <= 0) || (time1s == picktime2) || (result1==result2 && result1==0))
+                            {
+                                if ((checkBox1.Checked == true && checkBox2.Checked == true) || (checkBox1.Checked == false && checkBox2.Checked == false))
+                                {
+                                    if (textBox2.Text == String.Empty) {
+                                        listBox1.Items.Add(fi.ToString());
+                                    }
+                                    else if (dlit < Convert.ToInt32(textBox2.Text))
+                                    {
+                                        listBox1.Items.Add(fi.ToString());
+                                    }
+                                }
+                                else if (checkBox1.Checked == true && checkBox2.Checked == false)
+                                {
+                                    if (indexOfChar != -1)
+                                    {
+                                        if (textBox2.Text == String.Empty) {
+                                            listBox1.Items.Add(fi.ToString());
+                                        }
+                                        else if (dlit < Convert.ToInt32(textBox2.Text))
+                                        {
+                                            listBox1.Items.Add(fi.ToString());
+                                        }
+                                    }
+                                }
+                                else
+                                {
+                                    if (indexOfChar == -1)
+                                    {
+                                        if (textBox2.Text == String.Empty) {
+                                            listBox1.Items.Add(fi.ToString());
+                                        }
+                                        else if (dlit < Convert.ToInt32(textBox2.Text))
+                                        {
+                                            listBox1.Items.Add(fi.ToString());
+                                        }
+                                    }
+
+                                }
+                            }
+                        }
+                }
+                else
+                {
+                    int kolvo = Convert.ToInt32(textBox1.Text);
+                    foreach (FileInfo fi in files)
+                    {
+                        if (kolvo != 0)
+                        {
+                        string a = fi.ToString();
+                        int indexOfChar = a.IndexOf("QUEUE%");
+                        DateTime time1 = fi.CreationTime;
+                        string time1s = time1.ToShortDateString();
+                        var result1 = DateTime.Compare(time1, dateTimePicker1.Value);
+                        var result2 = DateTime.Compare(time1, dateTimePicker2.Value);
+                        string picktime2 = dateTimePicker2.Value.ToShortDateString();
+                        long dlit = fi.Length / 1024 / 1024;
+                        if ((result1 >= 0 && result2 <= 0) || (time1s == picktime2) || (result1 == result2 && result1 == 0))
+                            {
+                                if ((checkBox1.Checked == true && checkBox2.Checked == true) || (checkBox1.Checked == false && checkBox2.Checked == false))
+                                {
+                                    if (textBox2.Text == String.Empty)
+                                    {
+                                        listBox1.Items.Add(fi.ToString());
+                                        kolvo -= 1;
+                                    }
+                                    else if (dlit < Convert.ToInt32(textBox2.Text))
+                                    {
+                                        listBox1.Items.Add(fi.ToString());
+                                        kolvo -= 1;
+                                    }
+                                }
+                                else if (checkBox1.Checked == true && checkBox2.Checked == false)
+                                {
+                                    if (indexOfChar != -1)
+                                    {
+                                        if (textBox2.Text == String.Empty)
+                                        {
+                                            listBox1.Items.Add(fi.ToString());
+                                            kolvo -= 1;
+                                        }
+                                        else if (dlit < Convert.ToInt32(textBox2.Text))
+                                        {
+                                            listBox1.Items.Add(fi.ToString());
+                                            kolvo -= 1;
+                                        }
+
+                                    }
+                                }
+                                else
+                                {
+                                    if (indexOfChar == -1)
+                                    {
+                                        if (textBox2.Text == String.Empty)
+                                        {
+                                            listBox1.Items.Add(fi.ToString());
+                                            kolvo -= 1;
+                                        }
+                                        else if (dlit < Convert.ToInt32(textBox2.Text))
+                                        {
+                                            listBox1.Items.Add(fi.ToString());
+                                            kolvo -= 1;
+                                        }
+                                    }
+
+                                }
+                            }
+                    }
+                    }
+                }
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if (!Char.IsDigit(number) && number != 8)
+            {
+                e.Handled = true;
+            }
+        }
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if (!Char.IsDigit(number) && number != 8)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+            string filetext = richTextBox1.Lines[0];
+            DirectoryInfo dir = new DirectoryInfo(filetext);
+            FileInfo[] files = dir.GetFiles("*.wav");
+            int dlinamas = files.Length;
+            if (dlinamas != 0)
+            {
+                string selel = listBox1.SelectedItem.ToString();
+                foreach (FileInfo fi in files)
+                {
+                    if (fi.ToString() == selel)
+                    {
+                        System.Diagnostics.Process.Start(fi.FullName);
+                    }
+                }
+            }
+        }
+    }
+}
